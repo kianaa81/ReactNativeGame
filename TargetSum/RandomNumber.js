aec4c4eb4b1b5ddfdc03b25671585046cad50ed4
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 13:07:06 -0600

    Added import {Text, Stylesheet} and PropTypes Added static PropTypes Added StyleSheet to it

--- TargetSum/RandomNumber.js
+++ TargetSum/RandomNumber.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import {Text, StyleSheet} from 'react-native';
 
 class RandomNumber extends React.Component {
+   static PropTypes = {
+       number: PropTypes.number.isRequired,
+   };
+   
   render() {
-    <Text style={styles.random} key={index}>{randomNumber}</Text>
+      return(
+    <Text style={styles.random}>{this.props.number}</Text>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+random: {
+    backgroundColor: 'rgb(128, 87, 167)',
+    width: 110,
+    marginHorizontal: 25,
+    marginVertical: 25,
+    fontSize: 35,
+    textAlign: 'center',
+  },
+});  
+
 
 export default RandomNumber;
