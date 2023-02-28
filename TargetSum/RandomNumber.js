2cfda5307e9b9db6deed7041051d9a212b438715
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 12:49:58 -0600

    Imported from react Added class RandomNumber that returns a <Text> that shows random numbers

---
+++ TargetSum/RandomNumber.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class RandomNumber extends React.Component {
+  render() {
+    <Text style={styles.random} key={index}>{randomNumber}</Text>
+  }
+}
+
+export default RandomNumber;
