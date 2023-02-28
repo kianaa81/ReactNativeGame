7820a076f5f65e44ab5a4b44b3f938c9d39c12f4
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 11:31:18 -0600

    Added an Array to get random numbers so that users can choose from them to calculate the sum.

--- TargetSum/App.js
+++ TargetSum/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import Game from './Game';
 class App extends React.Component {
- 
   render() {
-    return (
-      <Game randomNumberCount={6}/>
-    );
+    return <Game randomNumberCount={6} />;
   }
 }
-
 
 export default App;
