b880c1e4c22748cc2dcdcd5da7488f4db595f577
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 15:27:17 -0600

    Added a style called selected to change the opacity for the selected buttons Added a line of code to change the opacity once selected the buttons in return()

--- TargetSum/RandomNumber.js
+++ TargetSum/RandomNumber.js
@@ -3,25 +3,26 @@ import PropTypes from 'prop-types';
 import {Text, TouchableOpacity, StyleSheet} from 'react-native';
 
 class RandomNumber extends React.Component {
-   static propTypes = {
-       number: PropTypes.number.isRequired,
-   };
-   
-   handlePress = () => {
+  static propTypes = {
+    number: PropTypes.number.isRequired,
+    isSelected: PropTypes.bool.isRequired,
+};
 
-   };
+  handlePress = () => {};
 
   render() {
-      return(
-          <TouchableOpacity onPress={this.handlePress}>
-                  <Text style={styles.random}>{this.props.number}</Text>
-          </TouchableOpacity>
+    return (
+      <TouchableOpacity onPress={this.handlePress}>
+        <Text style={[styles.random, this.props.isSelected && styles.selected]}>
+          {this.props.number}
+        </Text>
+      </TouchableOpacity>
     );
   }
 }
 
 const styles = StyleSheet.create({
-random: {
+  random: {
     backgroundColor: 'rgb(128, 87, 167)',
     width: 110,
     marginHorizontal: 25,
@@ -29,7 +30,11 @@ random: {
     fontSize: 35,
     textAlign: 'center',
   },
-});  
 
+  selected: {
+    opacity: 0.3,
+  },
+
+});
 
 export default RandomNumber;
