40cc9d6ceaa98fbceda46c649157220e56e05934
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 19:30:30 -0600

    Added an initialSeconds and set it to 10

--- TargetSum/App.js
+++ TargetSum/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Game from './Game';
 class App extends React.Component {
   render() {
-    return <Game randomNumberCount={6} />;
+    return <Game randomNumberCount={6} initialSeconds={10} />;
   }
 }
 
