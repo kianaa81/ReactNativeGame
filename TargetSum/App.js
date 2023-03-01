9e3efcb1d1a35cdaf968d6740c9705e837bfcc9a
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 20:55:59 -0600

    Added a key to the return()

--- TargetSum/App.js
+++ TargetSum/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Game from './Game';
 class App extends React.Component {
   render() {
-    return <Game randomNumberCount={6} initialSeconds={10} />;
+    return (<Game key={this.state.gameId} randomNumberCount={6} initialSeconds={10} />
+      );
   }
 }
 
