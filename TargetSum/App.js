66587ad854120362f71348f72f42f2fc85f4f2d7
DESKTOP-9JSI6HF\HP <salmasikiana@gmail.com>
2023-02-28 21:01:04 -0600

    Updated the file by adding a function named resetGame()

--- TargetSum/App.js
+++ TargetSum/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import Game from './Game';
 class App extends React.Component {
+  state = {
+    gameId: 1,
+  };
+  resetGame = () => {
+    this.setState((prevState) => {
+      return {gameId: prevState.gameId + 1};
+    });
+  }
   render() {
     return (<Game key={this.state.gameId} randomNumberCount={6} initialSeconds={10} />
       );
